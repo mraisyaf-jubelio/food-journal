b1c5f709d73e267063260fb7378cbc5d83941869
raisyaf20 <raisyafmuhamad@gmail.com>
2023-01-14 23:13:38 +0700

    modify feature update and modify size div col

--- src/component/user.jsx
+++ src/component/user.jsx
@@ -1,57 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Container, Row, Modal, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
 import { useForm } from "react-hook-form";
-import { uploadImage } from "./api";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { getUser, sesi, uploadImage } from "./api";
 import poto from "./assets/img/potoDefault.png";
+import axios from "axios";
 
 const User = () => {
   const [show, setShow] = useState(false);
-
+  const [data, setData] = useState([]);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const local = localStorage.getItem("myObject");
-  const obj = JSON.parse(local);
-  let sesi = {};
-  for (const i in obj) {
-    sesi = obj[i];
-  }
 
-  const schema = yup.object({
-    poto: yup
-      .mixed()
-      .required("You need to provide a file")
-      .test("fileSize", "the file is top large", (value) => {
-        return value && value[0].size <= 1000000;
-      })
-      .test("type", "not a picture", (value) => {
-        return (
-          value &&
-          (value[0].type === "image/jpeg" ||
-            value[0].type === "image/webp" ||
-            value[0].type === "image/png" ||
-            value[0].type === "application/jpg")
-        );
-      }),
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
-  const onSubmit = (data) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    let respon = data.defImg;
     const imgUser = data.poto[0];
-    console.log(data);
-    uploadImage(imgUser)
-      .then((respon) => console.log(respon))
-      .catch((eror) => console.log(eror));
+
+    if (data.poto) {
+      try {
+        respon = await uploadImage(imgUser);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    }
+    axios
+      .post(
+        `${process.env.REACT_APP_BASEURL}/api/v1/update-profile`,
+        {
+          name: data.name,
+          email: data.email,
+          profilePictureUrl: respon,
+          phoneNumber: data.num,
+        },
+        {
+          headers: {
+            apiKey: process.env.REACT_APP_APIKEY,
+            Authorization: `Bearer ${sesi.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        alert(response.data.message);
+        window.location.reload(false);
+      })
+      .catch((err) => console.log(err.response.data));
   };
 
+  useEffect(() => {
+    getUser().then((respon) => setData(respon));
+  }, []);
   return (
     <Container>
       <div className="poto">
@@ -60,24 +62,24 @@ const User = () => {
       <Row className="head-user justify-content-center">
         <Col md={5} sm={9} xs={9} className="body-user text-center p-3 p-sm-3">
           <div className="user-img text-center">
-            <img src={sesi.pict ? sesi.pict : poto} className="img-fluid" alt="poto User" />
+            <img src={data.profilePictureUrl ? data.profilePictureUrl : poto} className="img-fluid" alt="poto User" />
           </div>
           <div className="mt-3">
             <p className="fs-5">
               <FontAwesomeIcon icon={faUser} className="me-2" />
-              {sesi.name}
+              {data.name}
             </p>
           </div>
           <div className="mt-3">
             <p className="fs-5">
               <FontAwesomeIcon icon={faEnvelope} className="me-2" />
-              {sesi.email}
+              {data.email}
             </p>
           </div>
           <div className="mt-3">
             <p className="fs-5">
               <FontAwesomeIcon icon={faPhoneAlt} className="me-2" />
-              {sesi.num}
+              {data.phoneNumber}
             </p>
           </div>
           <Button onClick={handleShow} className="btn-light edit-user text-white">
@@ -95,20 +97,21 @@ const User = () => {
           <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicText">
               <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" {...register("name")} />
+              <Form.Control type="text" {...register("name")} defaultValue={data.name} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" {...register("email")} />
+              <Form.Control type="email" {...register("email")} defaultValue={data.email} />
             </Form.Group>
+            <Form.Control type="hidden" value={data.profilePictureUrl} {...register("defImg")} />
             <Form.Group controlId="formFile" className="mb-4">
               <Form.Label>Poto</Form.Label>
               <Form.Control {...register("poto")} type="file" name="poto" />
             </Form.Group>
-            {/* <p className="text-danger">{errors.poto?.message}</p> */}
+
             <Form.Group>
               <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="number" {...register("num")} />
+              <Form.Control type="number" {...register("num")} defaultValue={data.phoneNumber} />
             </Form.Group>
             <div className="d-flex justify-content-around mt-3">
               <Button variant="outline-light" className="back-color" type="submit">
